b37841770e056381a76fa41681ac661752c8e3ae
JASKARAN SINGH <jaskaransaggu20@gmail.com>
2025-07-24 22:06:51 +0530

    Update Queries.sql

--- queries.sql
+++ queries.sql
@@ -1,27 +1,27 @@
-## List all donor from delhi and pune
+## List all donor from delhi and pune.
 
 select donor_name from blood_donor
 where city_id IN (select city_id from city
 where city_name = 'Delhi' or city_name = 'pune');
 
-## Find all blood donor with blood group 'B+'
+## Find all blood donor with blood group 'B+'.
 
 select * from blood_donor
 where donor_blood_group = 'B+';
 
-## Display the information of Hospital_Info_1 handled by BB_Manager whose ID is 103: 
+## Display the information of Hospital_Info_1 handled by BB_Manager whose ID is 103.
 
 select * from hospital_information_1
 where manager_id = 103;
 
-## Show the donors having the same blood groups required by the recipient staying in the same city along with recipient details.---
+## Show the donors having the same blood groups required by the recipient staying in the same city along with recipient details.
 
 select donor_id , donor_name , recipient_id , recipient_name
 from blood_donor as bd inner join blood_recipient as br
 where donor_blood_group = recipient_blood_group 
 AND bd.city_id = br.city_id;
 
-## Find the most comman blood group among donors
+## Find the most comman blood group among donors.
 
 select donor_blood_group , count(*) as total
 from blood_donor
